713077adf33e87dfc496474a7d28be71ccd1ac05
Braian Vaylet <braian.vaylet@globant.com>
2024-02-16 14:39:44 -0300

    feat: asd

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@components/atoms/Button";
-
 const Root = ({ name }: { name: string }): JSX.Element => {
   return (
     <>
       <script src="/__ENV.js" defer />
       <section>
         <p>{name} is mounted!</p>
-        <Button label="Click me" />
-        <Button label="Click me 2" />
       </section>
     </>
   );
